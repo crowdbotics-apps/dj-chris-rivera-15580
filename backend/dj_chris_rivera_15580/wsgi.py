023d19ce7e3b42a426f6b0aceaab4122b7494d0e
Crowdbotics <team@crowdbotics.com>
2020-04-08 00:02:36 +0000

    Update models

--- backend/dj_chris_rivera_15580/wsgi.py
+++ backend/dj_chris_rivera_15580/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_chris_rivera_15580.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj_chris_rivera_15580.settings")
 
 application = get_wsgi_application()
